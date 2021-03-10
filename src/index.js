ab907f35f4a88892980d3bc8f84821461733c3f8
camyll31 <79679423+camyll31@users.noreply.github.com>
2021-03-10 12:23:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ async function inserir(obj) {
    let res = await Cliente.create(obj)
        console.log(res);
 }
-//let c1 = {nome: "Fulano", sobrenome: "da Silva", endereco: "rua da Misericórdia", telefone: "98754312"};
-//let c2 = {nome:"Marivalda", sobrenome: "Ferreira", endereco: "rua do Limoeiro", telefone: "87612309"};
-//let c3 = {nome:"Marinete", sobrenome: "Bonjoura", endereco: "rua da Tangerina", telefone: "01846672"};
-//let c4 = {nome:"Lorena", sobrenome: "Ritalva", endereco: "rua da Galinha", telefone: "93810462"};
+/*let c1 = {nome: "Fulano", sobrenome: "da Silva", endereco: "rua da Misericórdia", telefone: "98754312"};
+let c2 = {nome:"Marivalda", sobrenome: "Ferreira", endereco: "rua do Limoeiro", telefone: "87612309"};
+let c3 = {nome:"Marinete", sobrenome: "Bonjoura", endereco: "rua da Tangerina", telefone: "01846672"};
+let c4 = {nome:"Lorena", sobrenome: "Ritalva", endereco: "rua da Galinha", telefone: "93810462"};*/
 let c5 = {nome:"Fiona", sobrenome: "Sherebelde", endereco: "baixa da Égua", telefone: "16854287"};
 
+/*inserir(c1);
+inserir(c2);
+inserir(c3);
+inserir(c4);*/
 inserir(c5);
 
 async function consultar() {
